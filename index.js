04e915f39cc4540b781a9732b9a92a4aa84145f9
Aflah <aflahkhan.2020@gmail.com>
2021-03-07 02:51:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var Numer
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        alert("I got clicked");
+    })
+}
