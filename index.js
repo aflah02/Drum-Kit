2dc4b12720e8004eadbf40dfe313f937f259b7df
Aflah <aflahkhan.2020@gmail.com>
2021-03-07 03:25:11 +0530

    update

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var Numer
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("I got clicked");
+        this.style.color = "white";
     })
 }
+
+// var audio = new Audio('sounds/tom-1.mp3');
+//         audio.play();
